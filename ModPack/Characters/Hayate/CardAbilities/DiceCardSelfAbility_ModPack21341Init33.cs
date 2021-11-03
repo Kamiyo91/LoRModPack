f51abb3301be446a3830eecda68256ff77fbaefa
Kamiyo <kamiyo@outlook.it>
2021-11-03 23:12:31 +0100

    New Card Abilities

---
+++ ModPack/Characters/Hayate/CardAbilities/DiceCardSelfAbility_ModPack21341Init33.cs
@@ -0,0 +1,20 @@
+﻿namespace ModPack21341.Characters.Hayate.CardAbilities
+{
+    //Speed8Power2
+    public class DiceCardSelfAbility_ModPack21341Init33 : DiceCardSelfAbilityBase
+    {
+        public static string Desc =
+            "[On Use] If Speed is 8 or higher,all dice on this page gain +2 Power";
+
+        public override void OnUseCard()
+        {
+            if (card.speedDiceResultValue >= 8)
+            {
+                card.ApplyDiceStatBonus(DiceMatch.AllDice, new DiceStatBonus
+                {
+                    power = 2
+                });
+            }
+        }
+    }
+}
---
+++ ModPack/Characters/Mio/CardAbilities/DiceCardSelfAbility_ModPack21341Init34.cs
@@ -0,0 +1,20 @@
+﻿namespace ModPack21341.Characters.Hayate.CardAbilities
+{
+    //Speed6Power1
+    public class DiceCardSelfAbility_ModPack21341Init34 : DiceCardSelfAbilityBase
+    {
+        public static string Desc =
+            "[On Use] If Speed is 6 or higher,all dice on this page gain +1 Power";
+
+        public override void OnUseCard()
+        {
+            if (card.speedDiceResultValue >= 6)
+            {
+                card.ApplyDiceStatBonus(DiceMatch.AllDice, new DiceStatBonus
+                {
+                    power = 1
+                });
+            }
+        }
+    }
+}
